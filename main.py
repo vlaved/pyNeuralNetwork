f8cfa2cd50f25151b43691201a3c4347361e124a
Vladimir V <vlaved@gmail.com>
2023-08-29 14:22:36 +0800

    генерация матриц

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-import numpy
-import matplotlib.pyplot as plt
+import numpy as np
+
+# Матрицы весов
+weights_input_to_middle = np.random.uniform(-0.5, 0.5, (4, 5))
+weights_middle_to_output = np.random.uniform(-0.5, 0.5, (3, 4))
+
+# Нейрон смещения
+bias_input_to_middle = np.zeros((4, 1))
+bias_middle_to_output = np.zeros((3, 1))
